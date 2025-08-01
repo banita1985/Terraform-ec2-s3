e95b9e1f4432f404b988a691e0cb17d641c27f91
Banita Patri <101166105+banita1985@users.noreply.github.com>
2025-08-01 08:29:59 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source     = "./modules/vpc"
+  cidr_block = "10.0.0.0/16"
+}
+
+module "s3" {
+  source      = "./modules/s3"
+  bucket_name = "terraform-poc-bucket-${random_id.bucket_id.hex}"
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
+module "ec2" {
+  source        = "./modules/ec2"
+  ami_id        = var.ami_id
+  instance_type = "t2.micro"
+  subnet_id     = module.vpc.subnet_id
+}
