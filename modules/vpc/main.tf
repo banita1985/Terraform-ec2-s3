2e489c316a168cac0b5c96263061fb5cdfdbdf0a
Banita Patri <101166105+banita1985@users.noreply.github.com>
2025-08-01 11:28:55 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,11 +1,23 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
+
+  tags = {
+    Name       = "terraform-vpc"
+    CreatedBy  = "Terraform"
+    Project    = "POC29"
+  }
 }
 
 resource "aws_subnet" "subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "eu-central-1a"
+
+  tags = {
+    Name       = "terraform-subnet"
+    CreatedBy  = "Terraform"
+    Project    = "POC29"
+  }
 }
 
 output "vpc_id" {
