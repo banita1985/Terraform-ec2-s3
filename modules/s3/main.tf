63e8e3429b46809560981b42625f03c3148c4c7e
Banita Patri <101166105+banita1985@users.noreply.github.com>
2025-08-01 12:15:41 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,24 +1,18 @@
-variable "bucket_name" {
-  description = "The name of the S3 bucket to create"
-  default     = "terraform-created-bucket-banita-${random_id.bucket_suffix.hex}"
-}
-
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
+resource "aws_s3_bucket" "s3bucket" {
+  bucket = "terraform-created-bucket-banita-${random_id.bucket_suffix.hex}"
+  force_destroy = true
 
   tags = {
-    Name      = var.bucket_name
-    CreatedBy = "Terraform"
-    Owner     = "banita"
-    Purpose   = "State Storage"
+    Name        = "terraform-created-bucket-banita-${random_id.bucket_suffix.hex}"
+    Environment = "Dev"
+    CreatedBy   = "Terraform"
   }
 }
 
 output "bucket_name" {
-  value = aws_s3_bucket.bucket.id
+  value = aws_s3_bucket.s3bucket.bucket
 }
