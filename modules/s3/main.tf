66d15d645897d24f8c646f93cd7c946098935bd4
Banita Patri <101166105+banita1985@users.noreply.github.com>
2025-08-01 11:36:27 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,10 +1,19 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
-  acl    = "private"
+  force_destroy = true  # Optional: allows auto-deletion even if bucket is not empty
+
+  tags = {
+    Name        = "terraform-created-bucket"
+    Environment = "dev"
+    CreatedBy   = "Terraform"
+  }
 }
 
 output "bucket_name" {
   value = aws_s3_bucket.bucket.id
 }
 
-variable "bucket_name" {}
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
